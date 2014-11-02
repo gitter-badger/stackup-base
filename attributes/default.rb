ab0f570bbb23f01355d4ddddb072552a0408607b
Steven De Coeyer <git@stevendecoeyer.be>
2014-11-02 13:19:14 +0100

    Adds unattended upgrades
    This will make sure security bugs get patched. If the server needs a
    reboot, it will reboot automatically at 02:00AM. You can override these
    settings through attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,9 @@
 default['apt']['compile_time_update'] = true
 
 default['base']['extra_packages'] = []
+
+default['base']['unattended-upgrades']['Mail'] = 'foo@example.com'
+default['base']['unattended-upgrades']['MailOnlyOnError'] = 'true'
+default['base']['unattended-upgrades']['Automatic-Reboot'] = 'true'
+default['base']['unattended-upgrades']['Automatic-Reboot-Time'] = '02:00'
+default['base']['unattended-upgrades']['Dl-Limit'] = '70'
